a7f4dc50ac6956a77d6c2c8a6643bf2f222ac117
Jasurbek <jasurdev1604@gmail.com>
2023-09-20 14:34:09 +0500

    TypeScript

--- src/Root/index.jsx
+++ src/Root/index.jsx
@@ -4,7 +4,7 @@ export default function Root() {
   return (
     <React.Fragment>
       <h1>Jasurbek</h1>
-      <h1>kjsdnvjkdfnjknjkfnbknrj</h1>
+      <h1>Otelbayev</h1>
     </React.Fragment>
   );
 }
