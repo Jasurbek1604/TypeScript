7ffdbd3be8dcdf3808850f95d26d20586b604e70
Jasurbek <jasurdev1604@gmail.com>
2023-09-27 01:30:19 +0500

    TypeScript 1

--- src/lesson1/reviwe.js
+++ src/lesson1/reviwe.js
@@ -7,3 +7,5 @@
 // 	— TypeScript o'rganish uchun nimalar bilish talab qilinadi?
 // 	— TypeScript bugungi kunda qanchalik muhim?
 // 	— Ushbu video darslik nimlarni o'z ichiga oladi?
+
+// npm i typescript -g
