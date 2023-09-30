2a667b7144b6693fd3b2d9cdf12f22c4637497b6
Jasurbek <jasurdev1604@gmail.com>
2023-09-30 23:25:40 +0500

    TypeScript with React

--- lesson6/reviwe.js
+++
@@ -1,9 +0,0 @@
-//! 6 - dars | TypeScript with ReactJs
-// 	— ReactJs template with TS 
-// 	— React class hooks component explation
-// 	— Types of passing props
-// 	— Props with interfece and type
-// 	— Function props
-// 	— Children props ReactNode
-// 	— useState with TS
-// 	— useReducer with TS
