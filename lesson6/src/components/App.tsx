a857285121e93a62f6db6e391c212cf2221b41f0
Jasurbek <jasurdev1604@gmail.com>
2023-10-02 00:25:31 +0500

    React and TypeScript

---
+++ lesson6/src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <h1>Jasurbek O'telbayev</h1>
+    </React.Fragment>
+  );
+}
--- lesson6/src/main.tsx
+++ lesson6/src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./components/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>Jasurbek Otelbayev</h1>
+    <App />
   </React.StrictMode>
 );
