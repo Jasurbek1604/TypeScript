e7bb664a605015942758fb72a80590c57730133c
Jasurbek <jasurdev1604@gmail.com>
2024-01-21 01:02:11 +0500

    TypeScript Bascis

---
+++ second/lesson2/index.js
@@ -0,0 +1,23 @@
+"use strict";
+//!TypeScript Basics
+//!String
+let str = "Jasurbek";
+str = "noen";
+//!Number
+let num = 45;
+num = 50;
+//!Boolean
+let bool = true;
+bool = false;
+//!Null
+let n = null;
+n = null;
+//!Undefined
+let u = undefined;
+u = undefined;
+//!Object
+let obj = { name: "Jasurbek" };
+//!Any
+let any = "string";
+any = 50;
+any = {};
---
+++ second/lesson2/index.ts
@@ -0,0 +1,30 @@
+//!TypeScript Basics
+
+//!String
+let str: string = "Jasurbek";
+str = "noen";
+
+//!Number
+let num: number = 45;
+num = 50;
+
+//!Boolean
+let bool: boolean = true;
+bool = false;
+
+//!Null
+let n: null = null;
+n = null;
+
+//!Undefined
+let u: undefined = undefined;
+u = undefined;
+
+//!Object
+let obj: { name: string } = { name: "Jasurbek" };
+
+//!Any
+
+let any: any = "string";
+any = 50;
+any = {};
