a768bc95183fc3e7f0fe221035af6ec716d3e7ed
Jasurbek <jasurdev1604@gmail.com>
2024-01-21 19:12:59 +0500

    Classes

---
+++ second/lesson6/index.js
@@ -0,0 +1,37 @@
+"use strict";
+//!Classes
+class Person {
+    static desc() {
+        return `bu class pesron`;
+    }
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+    info() {
+        return `${this._name}`;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    get age() {
+        return this._age;
+    }
+    set age(value) {
+        this._age = value;
+    }
+}
+Person.isFly = false;
+const jasur = new Person("Jasurbek", 20);
+const none = new Person("None", 100);
+// console.log(jasur.info());
+// console.log(Person.desc());
+console.log(jasur._name);
+console.log(jasur._age);
+jasur.name = "none";
+jasur.age = 15;
+console.log(jasur._name);
+console.log(jasur.age);
---
+++ second/lesson6/index.ts
@@ -0,0 +1,47 @@
+//!Classes
+
+class Person {
+  _name: string;
+  _age: number;
+  static isFly: boolean = false;
+  static desc(): string {
+    return `bu class pesron`;
+  }
+
+  constructor(name: string, age: number) {
+    this._name = name;
+    this._age = age;
+  }
+  info(): string {
+    return `${this._name}`;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+  set name(value: string) {
+    this._name = value;
+  }
+
+  get age(): number {
+    return this._age;
+  }
+  set age(value: number) {
+    this._age = value;
+  }
+}
+
+const jasur: Person = new Person("Jasurbek", 20);
+const none: Person = new Person("None", 100);
+
+// console.log(jasur.info());
+// console.log(Person.desc());
+
+console.log(jasur._name);
+console.log(jasur._age);
+
+jasur.name = "none";
+jasur.age = 15;
+
+console.log(jasur._name);
+console.log(jasur.age);
